5d86345b22f3ce8b091f0fcfac59298695b9ed07
Sadab Masrur <sadabmasrur08@gmail.com>
2025-02-10 21:56:24 +0600

    acp

---
+++ acp.py
@@ -0,0 +1,21 @@
+import math
+
+
+
+print(math.sin(45))
+print(math.cos(45))
+print(math.tan(45))
+ 
+ 
+print(math.ceil(45.490))
+print(math.floor(45.890))
+print(math.factorial(5))
+
+
+a = math.sin(0.5)
+b = math.cos(0.5)
+c = math.tan(0.5)
+
+print("value of sin(0.5)= ", a)
+print("value of cos(0.5)= ", b)
+print("value of tan(0.5)= ", c)
